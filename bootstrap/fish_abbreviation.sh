cb18e9bb101fdb9a2024f1ceada349e06887d9d8
SabariVig <hey@lxz.io>
2021-02-16 00:40:37 +0530

    adds fish abbr for docker and git and other misc

--- bootstrap/fish_abbreviation.sh
+++ bootstrap/fish_abbreviation.sh
@@ -1,25 +1,35 @@
 #!/bin/fish
+
+# Git
 abbr -Ua g "git"
-abbr -Ua gc "git commit"
-abbr -Ua ga "git add -U"
-abbr -Ua gg "git checkout"
-abbr -Ua gb "git branch"
 abbr -Ua gp "git push"
-abbr -Ua gs "git status"
-abbr -Ua gt "git status"
 abbr -Ua gl "git logs"
-
-abbr -Ua gpl "git pull"
 abbr -Ua gd "git diff"
+abbr -Ua gpl "git pull"
+abbr -Ua gb "git branch"
+abbr -Ua ga "git add -U"
+abbr -Ua gc "git commit"
+abbr -Ua gs "git status"
+abbr -Ua gg "git checkout"
 abbr -Ua gdd "git diff HEAD"
+abbr -Ua gt "git log --graph --oneline --all"
 
-abbr -Ua SS "sudo systemctl"
+
+# System
 abbr -Ua p "sudo pacman -S"
+abbr -Ua SS "sudo systemctl"
 
+# Docker
 abbr -Ua d "docker"
+abbr -Ua dp "docker ps -a"
+abbr -Ua ds "sudo systemctl start docker"
+
 abbr -Ua dra "docker rm (docker ps -aq)"
 abbr -Ua dri "docker rmi (docker images -aq)"
 
-
-
+# Kubernetes
 abbr -Ua k "kubernetes"
+
+# Misc 
+abbr -Ua se "export (cat .env |xargs -L 1)"
+abbr -Ua vim "nvim"
