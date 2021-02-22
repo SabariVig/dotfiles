72409f7f867b0fbadf3cb8321e12eba997d313f3
SabariVig <hey@lxz.io>
2021-02-22 23:24:52 +0530

    adds ansible alias

--- bootstrap/fish_abbreviation.sh
+++ bootstrap/fish_abbreviation.sh
@@ -7,12 +7,13 @@ abbr -Ua gl "git logs"
 abbr -Ua gd "git diff"
 abbr -Ua gpl "git pull"
 abbr -Ua gb "git branch"
-abbr -Ua ga "git add -U"
+abbr -Ua ga "git add -u"
 abbr -Ua gc "git commit"
 abbr -Ua gs "git status"
 abbr -Ua gg "git checkout"
 abbr -Ua gdd "git diff HEAD"
 abbr -Ua gt "git log --graph --oneline --all"
+abbr -Ua gr "git restore --staged"
 
 
 # System
@@ -30,6 +31,19 @@ abbr -Ua dri "docker rmi (docker images -aq)"
 # Kubernetes
 abbr -Ua k "kubernetes"
 
+# Ansible 
+abbr -Ua a "ansilbe"
+abbr -Ua ad "ansilbe-doc"
+abbr -Ua ap "ansilbe-playbook -i "
+
+# Terraform 
+abbr -Ua tf "terraform"
+abbr -Ua tfp "terraform plan"
+abbr -Ua tff "terraform fmt"
+
+
 # Misc 
 abbr -Ua se "export (cat .env |xargs -L 1)"
 abbr -Ua vim "nvim"
+abbr -Ua rmd "rm -rf"
+
