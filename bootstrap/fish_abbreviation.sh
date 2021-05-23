768e2c566dabbcb54185154b5c579fdd987ac8a0
SabariVig <hey@lxz.io>
2021-05-24 02:57:12 +0530

    fix fish abbr

--- bootstrap/fish_abbreviation.sh
+++ bootstrap/fish_abbreviation.sh
@@ -2,17 +2,19 @@
 
 # Git
 abbr -Ua g "git"
-abbr -Ua gp "git push"
+abbr -Ua gp "git push origin"
 abbr -Ua gl "git logs"
 abbr -Ua gd "git diff"
 abbr -Ua gpl "git pull"
 abbr -Ua gb "git branch"
-abbr -Ua ga "git add -U"
+abbr -Ua ga "git add -u"
 abbr -Ua gc "git commit"
 abbr -Ua gs "git status"
 abbr -Ua gg "git checkout"
 abbr -Ua gdd "git diff HEAD"
 abbr -Ua gt "git log --graph --oneline --all"
+abbr -Ua grt "git restore"
+abbr -Ua gr "cd (git rev-parse --show-toplevel)"
 
 
 # System
@@ -23,13 +25,18 @@ abbr -Ua SS "sudo systemctl"
 abbr -Ua d "docker"
 abbr -Ua dp "docker ps -a"
 abbr -Ua ds "sudo systemctl start docker"
-
 abbr -Ua dra "docker rm (docker ps -aq)"
 abbr -Ua dri "docker rmi (docker images -aq)"
+abbr -Ua dst "docker stop (docker ps -aq)"
+
+# Docker Compose
+abbr -Ua dc "docker-compose"
+abbr -Ua dcp "docker-compose up -d"
+abbr -Ua dcb "docker-compose --build up -d"
 
 # Kubernetes
-abbr -Ua k "kubernetes"
+abbr -Ua k "kubectl"
 
 # Misc 
-abbr -Ua se "export (cat .env |xargs -L 1)"
+abbr -Ua xe "export (cat .env |xargs -L 1)"
 abbr -Ua vim "nvim"
